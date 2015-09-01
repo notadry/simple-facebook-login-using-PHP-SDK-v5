6061cd25830a44848531a549fa423634b98ce6cc
Adry Muhammad <heyadry@yahoo.com>
2015-09-01 15:11:29 +0700

    Change Config
    Change log #1
    
    1. dbconfig.php
    2. fbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,8 +1,9 @@
 <?php
-    define('DB_SERVER', 'localhost');
-    define('DB_USERNAME', 'root');           // DB username
-    define('DB_PASSWORD', '');              // DB password
-    define('DB_DATABASE', 'fblogin');      // DB name
+    define('DB_SERVER', 'DB SERVER');       // DB server
+    define('DB_USERNAME', 'DB USERNAME');   // DB username
+    define('DB_PASSWORD', 'DB PASSWORD');   // DB password
+    define('DB_DATABASE', 'DB NAME');       // DB name
+
     $conn = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die( "Unable to connect");
     $database = mysql_select_db(DB_DATABASE) or die( "Unable to select database");
 ?>
--- fbconfig.php
+++ fbconfig.php
@@ -3,15 +3,15 @@
 require_once __DIR__ . '/src/Facebook/autoload.php';
 
 
-$appID   = "730641963707408";   // APP ID
-$appSecret = "fbd47cf51700fff96bbbef30e8caa3bd";    // APP SECRET
-$my_url ="http://localhost/fblogin";   // APP DOMAIN --> ini harus disetting di facebook app juga
+$appID      = "YOUR APP ID";   // APP ID
+$appSecret  = "YOUR APP SECRET";    // APP SECRET
+$my_url     = "YOUR APP DOMAIN";   // APP DOMAIN --> It has to be match with app domain on your facebook app
  
 
  $fb = new Facebook\Facebook([
   'app_id' => $appID,
   'app_secret' => $appSecret,
-  'default_graph_version' => 'v2.4',
+  'default_graph_version' => 'v2.4', // API GRAPH VERSION
   ]);
 
 

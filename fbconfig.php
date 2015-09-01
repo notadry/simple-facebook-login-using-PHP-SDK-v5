36b2db531b847cd0cf2407a81566ec131f5f56b2
Adry Muhammad <heyadry@yahoo.com>
2015-09-01 15:16:30 +0700

    Simple Facebook Login using PHP SDK v5
    1 . delete fblogin.zip
    2. change fbconfig.php

--- fbconfig.php
+++ fbconfig.php
@@ -3,9 +3,9 @@
 require_once __DIR__ . '/src/Facebook/autoload.php';
 
 
-$appID      = "YOUR APP ID";   // APP ID
+$appID      = "YOUR APP ID";        // APP ID
 $appSecret  = "YOUR APP SECRET";    // APP SECRET
-$my_url     = "YOUR APP DOMAIN";   // APP DOMAIN --> It has to be match with app domain on your facebook app
+$my_url     = "YOUR APP DOMAIN";    // APP DOMAIN --> It has to be match with app domain on your facebook app
  
 
  $fb = new Facebook\Facebook([
